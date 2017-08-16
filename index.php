c846126a32f0e019ee184dd827340ef866c58c20
Henrique Pappis <falecom@henriquepappis.com>
2017-08-16 08:22:54 -0300

    inicio do index placa cirio

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sua Placa Mauricio Cirio</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div class="container">
+		<h1>Sua Placa Mauricio Cirio</h1>
+		<p>Faça sua placa</p>
+	</div>
+</body>
+</html>
