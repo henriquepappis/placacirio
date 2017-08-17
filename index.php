e7d65666b6cce644e141c0bfb61b024790de8a00
Henrique Pappis <henrique.pappis@simers.org.br>
2017-08-17 17:30:28 -0300

    Subindo prototipos

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-	<link href="https://cdn.quilljs.com/1.3.1/quill.snow.css" rel="stylesheet">
+	<link href="http://www.jqueryrain.com/wp-content/plugins/wp-bar/wpbar.css" rel="stylesheet" type="text/css">
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	<script src="https://cdn.quilljs.com/1.3.1/quill.js"></script>
 </head>
@@ -16,9 +16,10 @@
 	</div>
 
 	<!-- Create the editor container -->
-	<div id="editor">
-		<p>Hello World!</p>
-		<p>Some initial <strong>bold</strong> text</p>
+	<div id="editor" class="md-6">
+		<p>Olá amiguinho, deixe sua mensagem para esse rico rapaz. E, aproveite para curtir seu canal.</p>
+		<p><a href="https://www.youtube.com/user/ciriotv" target="_blank">CirioTV</p>
+		<p><a href="https://www.youtube.com/playlist?list=PLxp7pD6wbCLFo7fQD4n_tdYeNZ7r79BQN" target="_blank">Cantadas Crônicas</p>
 		<p><br></p>
 	</div>
 
