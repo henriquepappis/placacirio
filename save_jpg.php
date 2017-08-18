8ebcf9aacae347fa7ce027ed10cdc9a626a046ac
Henrique Pappis <henrique.pappis@simers.org.br>
2017-08-18 18:31:26 -0300

    Ajustes responsividade

--- save_jpg.php
+++ save_jpg.php
@@ -1,8 +1,13 @@
 <?php
 	$imagedata = base64_decode($_POST['imgdata']);
+
 	$filename = md5(uniqid(rand(), true));
+
 	//path where you want to upload image
 	$file = 'saves/'.$filename.'.png';
+
 	$imageurl  = 'saves/'.$filename.'.png';
+
 	file_put_contents($file,$imagedata);
+
 	echo $imageurl;
